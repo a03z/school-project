9988239c296a496bd54ba664f388adaa8b75f166
Daniil <a03zcode@yandex.ru>
2021-04-13 23:04:28 +0300

    Update top-btn.js

--- js/top-btn.js
+++ js/top-btn.js
@@ -3,20 +3,20 @@ const goTopBtn = document.getElementById('top')
 window.addEventListener('scroll', trackScroll)
 goTopBtn.addEventListener('click', backToTop)
 
-function backToTop () {
-  if (window.pageYOffset > 0) {
-    window.scrollTo({ top: 0, behavior: 'smooth' })
-  }
+const backToTop = () => {
+    if (window.pageYOffset > 0) {
+        window.scrollTo({ top: 0, behavior: 'smooth' })
+    }
 }
 
-function trackScroll () {
-  const scrolled = window.pageYOffset
-  const coords = document.documentElement.clientHeight
+const trackScroll = () => {
+    const scrolled = window.pageYOffset
+    const coords = document.documentElement.clientHeight
 
-  if (scrolled > coords) {
-    goTopBtn.classList.add('top-show')
-  }
-  if (scrolled < coords) {
-    goTopBtn.classList.remove('top-show')
-  }
+    if (scrolled > coords) {
+        goTopBtn.classList.add('top-show')
+    }
+    if (scrolled < coords) {
+        goTopBtn.classList.remove('top-show')
+    }
 }
